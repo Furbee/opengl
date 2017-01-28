6a228ef31afae1d6836da5890bf96d837bd27dc7
Oscar Nord <oscar.nord@gmail.com>
2017-01-28 23:38:14 +0100

    added files

---
+++ cmake-build-debug/CMakeFiles/opengl_new.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Oscar/Documents/opengl_new/main.cpp" "/Users/Oscar/Documents/opengl_new/cmake-build-debug/CMakeFiles/opengl_new.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  "TW_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../external/glfw/include"
+  "../external/glfw/deps"
+  "../external/glm"
+  "../external/glew/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/Oscar/Documents/opengl_new/cmake-build-debug/external/glew/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/Users/Oscar/Documents/opengl_new/cmake-build-debug/CMakeFiles/glew.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/opengl_new.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opengl_new.dir/main.cpp.o"
+  "../build/opengl_new.pdb"
+  "../build/opengl_new"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opengl_new.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
